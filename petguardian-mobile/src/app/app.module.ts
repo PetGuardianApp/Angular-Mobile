e3d670cb3c9a275e26703707d4112499b1656193
David Castro Sanchez <david.castro.sachez@gmail.com>
2023-10-31 19:15:42 +0100

    Firebase connected

--- petguardian-mobile/src/app/app.module.ts
+++ petguardian-mobile/src/app/app.module.ts
@@ -1,5 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFireStorageModule } from '@angular/fire/compat/storage';
+import { environment } from '../enviroments/enviroments';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
