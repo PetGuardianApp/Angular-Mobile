cc56388594278805a6a2f1f35406ebb05a130e07
Barbera99 <josep.barbera.reverte.1999@gmail.com>
2023-11-06 19:13:19 +0100

    Creació del component

--- petguardian-mobile/src/app/app.module.ts
+++ petguardian-mobile/src/app/app.module.ts
@@ -9,11 +9,13 @@ import { environment } from '../enviroments/enviroments';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
