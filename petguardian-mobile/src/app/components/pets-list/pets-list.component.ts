082f6432460eb5fe389a654a9503bcd40ed44de9
JoelAmpurda <ampurda98@gmail.com>
2023-11-13 19:58:46 +0100

    Modified the style for the pets list

--- petguardian-mobile/src/app/components/pets-list/pets-list.component.ts
+++ petguardian-mobile/src/app/components/pets-list/pets-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { PetModel } from 'src/app/models/pet.model';
 import { ApiService } from 'src/app/services/api.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-pets-list',
@@ -10,7 +11,7 @@ import { ApiService } from 'src/app/services/api.service';
 export class PetsListComponent {
   petModel: PetModel;
   savedPets: any[] = [];
-  constructor(public apiService: ApiService) { 
+  constructor(public apiService: ApiService, private router: Router, ) { 
     this.petModel = new PetModel;
     this.showData();
   }
@@ -29,38 +30,16 @@ export class PetsListComponent {
     }
   }
 
+  redirectPetPage(id: string) {
+    this.router.navigate(['pet-profile'], {
+      queryParams: { petId: id }
+    });
+  }
+
   registerPet(): void {
     // Code for registering a new pet (same as before)
-
-    // Update the savedPets array with an instance of PetModel
-    // this.petsArray.push(this.petModel);
-    // const requestBody: PetModel = {
-    //   birth: '',
-    //   breed: '',
-    //   weight: {},
-    //   health_info: {
-    //     cardiac_freq: {},
-    //     vaccines: [],
-    //     observations: '',
-    //     steps: {}
-    //   },
-    //   height: 0,
-    //   name: '', // Use provided name or empty string if not provided
-    //   type: '', // Use provided type or empty string if not provided
-    //   vet_id: '',
-    //   client_id: '',
-    //   profile_image: '',
-    //   id: '',
-    //   ...this.petModel // Spread provided pet object to override default values if they are provided
-    // };    
-    // // const requestBody: PetModel = { ...this.petModel }; // Use object spread to create a shallow copy of the pet object    
-    // console.log(requestBody);
-    // // Store the updated pet data in session storage
-    // sessionStorage.setItem('pets', JSON.stringify(this.petsArray));
     this.petModel.client_id = "VPUnbME8Kt27zmF7q7ne"
     this.apiService.postClientPets(this.petModel);
-    location.reload
-
     // Close the form
     this.closeForm();
   }
@@ -68,6 +47,13 @@ export class PetsListComponent {
   showData(): void {
     this.apiService.getClientPets("VPUnbME8Kt27zmF7q7ne").then((petsArray) => {
       this.apiService.petsArray = petsArray;
+      for (const pet of this.apiService.petsArray) {
+        if (pet.profile_image == '') {
+          pet.profile_image = '/assets/img/dogImage1.jpg';
+        } else{
+          pet.profile_image = '/assets/img/dogImage2.jpg';
+        }
+      }
       console.log(petsArray);
     })
   }
