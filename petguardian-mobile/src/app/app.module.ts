df655f1ce6fd90ee7720c2e7fdfc778a2c76fb1e
Pau Llobet <pllobet@mychefpro.com>
2023-11-15 20:16:13 +0100

    Fix: navBar and footer

--- petguardian-mobile/src/app/app.module.ts
+++ petguardian-mobile/src/app/app.module.ts
@@ -20,7 +20,6 @@ import { SideNavComponent } from './components/side-nav/side-nav.component';
 import { SidenavLinkComponent } from './components/sidenav-link/sidenav-link.component';
 import { MapPageComponent } from './components/map-page/map-page.component';
 import { HttpClientModule } from '@angular/common/http';
-import { GoogleMapsModule } from '@angular/google-maps';
 import { TopBarComponent } from './components/top-bar/top-bar.component';
 import { RegisterComponent } from './components/register/register.component';
 import { AppointmentsComponent } from './components/appointments/appointments.component';
